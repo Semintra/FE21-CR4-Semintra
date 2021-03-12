53fab54bb3a2012557c147232350ba1b33460c01
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-12 20:00:17 +0100

    add sort button and function

---
+++ scripts/btn_sort.js
@@ -0,0 +1,11 @@
+// ### Sort-function ###
+
+document.getElementById("sort").addEventListener("click", sortBtn);
+
+function sortBtn() {
+  movies.sort((a, b) => b.likes - a.likes);
+  const newCards = document.getElementById("content");
+  newCards.textContent = "";
+  movieCards();
+  btnLikes();
+}

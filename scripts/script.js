a689e709f41610d9da9329de9c4abbe5ce155c61
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-12 12:13:51 +0100

    add bootstrap cards + javascript code

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,10 @@ var content = document.getElementById("content");
 
 function movieCards() {
   for (i = 0; i < movies.length; i++) {
-    content.innerHTML += "<div>" + movies[i].movieName + "</div>";
+    content.innerHTML +=
+      '<div class="card col-sm-5 col-11 mx-auto">' +
+      movies[i].movieName +
+      "</div>";
   }
 }
 

5774fb946f4875e591d0348efae8ecfd8033de42
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-12 19:21:11 +0100

    complete like button script

--- scripts/btn_like.js
+++ scripts/btn_like.js
@@ -4,13 +4,14 @@ function btnLikes() {
   for (let i in movies) {
     var buttonCount = document.getElementsByClassName("card");
     var countTarget = document.getElementsByClassName("liCount");
-    buttonCount[i].innerHTML += "<button> Like me (" + i + ")</button>";
-    buttonCount[i].addEventListener("click", addLike);
+    buttonCount[i].innerHTML += "<button class='blike'>Like me</button>";
+    var bcount = document.getElementsByClassName("blike");
+    bcount[i].addEventListener("click", addLike);
     function addLike() {
       let up = parseInt(movies[i].likes) + 1;
       countTarget[i].innerHTML = up;
       movies[i].likes = up;
-      console.log(up);
+      //JSON.stringify();             // ### This was just a test (too dangerous ^^) ###
       console.log(movies[i].likes);
     }
   }

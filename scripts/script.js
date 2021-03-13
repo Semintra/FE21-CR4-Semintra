fc007a9f94c984d57fa01618ce2a3966e2f2ad49
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-13 15:24:12 +0100

    fix ranking output

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,9 @@ var content = document.getElementById("content");
 function movieCards() {
   for (i = 0; i < movies.length; i++) {
     content.innerHTML +=
-      "<div class='card col-sm-5 mb-3 mx-auto' style='max-width: 540px'><div class='row g-0'><div class='inCard1 rounded-start liButton col-md-4 ps-2 pt-2'><img src='" +
+      "<div class='card col-sm-5 mb-3 mx-auto' style='max-width: 540px'><div class='row g-0'><div id='rCard" +
+      [i] +
+      "' class='inCard1 rounded-start liButton col-md-4 ps-2 pt-2'><img src='" +
       movies[i].image +
       "' class='img-fluid'></div><div class='inCard2 rounded-end col-md-8'><div class='card-body'><h5 class='card-title'>" +
       movies[i].movieName +
@@ -25,6 +27,21 @@ function movieCards() {
       "<div>likes</div><div class='liCount'>" + // ### Like-button counter target ###
       movies[i].likes +
       "</div></div></div></div></div>";
+  }
+
+  // ### Checking if the movies' ranks ###
+
+  if (movies[0].likes > 0) {
+    document.getElementById("rCard0").innerHTML +=
+      "<img src='./images/rank_1.png'>";
+  }
+  if (movies[1].likes > 0 && movies[1].likes <= movies[0].likes) {
+    document.getElementById("rCard1").innerHTML +=
+      "<img src='./images/rank_2.png'>";
+  }
+  if (movies[2].likes > 0 && movies[2].likes <= movies[1].likes) {
+    document.getElementById("rCard2").innerHTML +=
+      "<img src='./images/rank_3.png'>";
   }
 }
 
